51f6b71936b6ff29fc737908a26bc49c9a4a2629
melisgb <gloriamelissa1@hotmail.com>
2021-04-17 19:03:22 +0100

    Defining the winner of the game
    Comparing the array of squares to see who the winner is.

--- src/index.js
+++ src/index.js
@@ -38,9 +38,15 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-  
-      return (
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if(winner) {
+            status = 'Winner: ' + winner;
+        }
+        else {
+            status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
+        return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
@@ -86,3 +92,25 @@ function Square(props) {
     document.getElementById('root')
   );
   
+
+
+  function calculateWinner(squares){
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]; //This is the possible combinations (lines) to win
+
+    for (let i = 0; i< lines.length; i++){
+        const[a,b,c] = lines[i];
+        if(squares[a] === squares[b] && squares[a] === squares[c] && squares[a]){
+            return squares[a];
+        }
+    }
+    return null;
+  }
